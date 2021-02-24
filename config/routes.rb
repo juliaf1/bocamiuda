2133bc0a391dce3211af5d979304be0d796ab906
julia frederico <magalhaesjulia@id.uff.br>
2021-02-23 21:54:43 -0300

    set up single user system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: "registrations" }
   resources :artists, only: [ :index, :show ]
   root to: 'artists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
