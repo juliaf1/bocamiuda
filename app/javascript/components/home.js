f68920c3a590b7f614e9a34d38efe1f02ce2b12f
julia frederico <magalhaesjulia@id.uff.br>
2021-02-27 16:45:23 -0300

    artists cards light up on home page

--- app/javascript/components/home.js
+++ app/javascript/components/home.js
@@ -5,18 +5,38 @@ const home = () => {
     categoryCards.forEach((card) => {
       card.addEventListener('click', () => {
         let categoryIcon = card.querySelector('img');
+        let artistsCards = document.querySelectorAll(`.${card.id}`);
 
         if (!(categoryIcon.style.display == "block")) {
           categoryIcon.style.display = "block";
-          card.classList.toggle("active");
+          card.classList.toggle("tag-active");
         } else {
           categoryIcon.style.display = "none";
-         card.classList.toggle("active");
+          card.classList.toggle("tag-active");
         };
+
+        artistsCards.forEach((acard) => {
+          let artistIcon = acard.querySelector('img');
+
+          if (!(artistIcon.style.display == "block")) {
+            artistIcon.style.display = "block";
+            acard.classList.toggle("artist-active");
+          } else {
+            artistIcon.style.display = "none";
+            acard.classList.toggle("artist-active");
+          };
+        });
 
       });
     });
   };
+
+  document.addEventListener('mousedown', function (event) {
+    if (event.detail > 1) {
+      event.preventDefault();
+      // preventing text selection on double click
+    }
+  }, false);
 }
 
 export { home };
